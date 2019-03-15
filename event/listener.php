b3866238946e207ad7e7d329153db0c634fa1189
Giovanni Dose <giannidose@tin.it>
2019-03-15 10:50:27 +0100

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -2,7 +2,7 @@
 /**
 *
 * Topic List extension for the phpBB Forum Software package.
-*
+* version 1.0.2
 * @copyright (c) 2018 Giovanni Dose (Micogian)
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
