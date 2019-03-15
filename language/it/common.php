e1005b5ec45beb45e4978a1e113e7604d9167a07
Giovanni Dose <giannidose@tin.it>
2019-03-15 10:52:39 +0100

    Update common.php

--- language/it/common.php
+++ language/it/common.php
@@ -2,7 +2,7 @@
 /** 
 * 
 * @package StaffIt - Topic List
-* @copyright (c) 2014 brunino
+* @copyright (c) 2018 micogian
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2 
 * 
 */ 
@@ -16,10 +16,10 @@ if (empty($lang) || !is_array($lang))
 }
 $lang = array_merge($lang, array(
 	'L_TOPIC_TITLE'			=> 'Titolo',
-	'L_TOPIC_AUTHOR'			=> 'Autore',
+	'L_TOPIC_AUTHOR'		=> 'Autore',
 	'L_TOPIC_DATE'			=> 'Data',
-	'L_REPLIES'				=> 'Risposte',
-	'L_VIEWS'				=> 'Visite',
+	'L_REPLIES'			=> 'Risposte',
+	'L_VIEWS'			=> 'Visite',
 	'TITLE_TOPIC_LIST'		=> 'LISTA ALFABETICA DEI TOPICS DEI FORUM',
 	'TEXT_TIME_1'			=> 'TUTTO',
 	'TEXT_TIME_2'			=> '7 giorni',
